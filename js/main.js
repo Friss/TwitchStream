c4fc0b8520ea262551ccb4126ba9895ba0fb1b2b
Zachary Friss <friss@frissdesigns.com>
2016-06-04 19:54:02 -0400

    Always display follower count

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 $.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?callback=?", function(response) {
   if (response.stream){
     $('.viewers span').text(response.stream.viewers);
-    $('.followers span').text(response.stream.channel.followers);
     $('.intro iframe').removeClass("hidden");
     $('.intro-text').addClass('hidden');
   }else{
     $('.viewers span').text("0");
-    $('.followers span').text("0");
   }
+});
+
+$.getJSON( "https://api.twitch.tv/kraken/channels/twitchusername/follows?callback=?", function(response) {
+  $('.followers span').text(response["_total"]);
 });
