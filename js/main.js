6218d30685716055352c5c7aa7a5d40674eeb01d
Zachary Friss <friss@frissdesigns.com>
2016-10-17 17:54:09 -0400

    Start ending auth token with requests.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-$.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?callback=?", function(response) {
+// Change this to be your own clientId from https://www.twitch.tv/kraken/oauth2/clients/new
+var clientId = "a7aag1oiocd1vxl0h9jvd2sahmaq5j0";
+
+$.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?client_id="+ clientId + "callback=?", function(response) {
   if (response.stream){
     $('.viewers span').text(response.stream.viewers);
     $('.intro iframe').removeClass("hidden");
@@ -8,6 +11,6 @@ $.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?callback=?", fun
   }
 });
 
-$.getJSON( "https://api.twitch.tv/kraken/channels/twitchusername/follows?callback=?", function(response) {
+$.getJSON( "https://api.twitch.tv/kraken/channels/twitchusername/follows?client_id="+ clientId + "callback=?", function(response) {
   $('.followers span').text(response["_total"]);
 });
