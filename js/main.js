db031aaf2a35e95767ab9d45507a903e527b677e
Zachary Friss <friss@frissdesigns.com>
2016-10-17 17:55:39 -0400

    Update with new authed endpoint. Switch to MOONMOON

--- js/main.js
+++ js/main.js
@@ -1,11 +1,15 @@
-$.getJSON( "https://api.twitch.tv/kraken/streams/seriousgaming?callback=?", function(response) {
+var clientId = "a7aag1oiocd1vxl0h9jvd2sahmaq5j0";
+
+$.getJSON( "https://api.twitch.tv/kraken/streams/moonmoon_ow?client_id="+ clientId + "callback=?", function(response) {
   if (response.stream){
     $('.viewers span').text(response.stream.viewers);
-    $('.followers span').text(response.stream.channel.followers);
     $('.intro iframe').removeClass("hidden");
     $('.intro-text').addClass('hidden');
   }else{
     $('.viewers span').text("0");
-    $('.followers span').text("0");
   }
+});
+
+$.getJSON( "https://api.twitch.tv/kraken/channels/moonmoon_ow/follows?client_id="+ clientId + "callback=?", function(response) {
+  $('.followers span').text(response["_total"]);
 });
