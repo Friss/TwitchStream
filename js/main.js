fb56aac13fff353cd686a46c5dd8eb151fd195f0
Zachary Friss <friss@frissdesigns.com>
2016-10-19 01:22:45 -0400

    Fix api url params

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 // Change this to be your own clientId from https://www.twitch.tv/kraken/oauth2/clients/new
 var clientId = "a7aag1oiocd1vxl0h9jvd2sahmaq5j0";
 
-$.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?client_id="+ clientId + "callback=?", function(response) {
+$.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?client_id="+ clientId + "&callback=?", function(response) {
   if (response.stream){
     $('.viewers span').text(response.stream.viewers);
     $('.intro iframe').removeClass("hidden");
@@ -11,6 +11,6 @@ $.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?client_id="+ cli
   }
 });
 
-$.getJSON( "https://api.twitch.tv/kraken/channels/twitchusername/follows?client_id="+ clientId + "callback=?", function(response) {
+$.getJSON( "https://api.twitch.tv/kraken/channels/twitchusername/follows?client_id="+ clientId + "&callback=?", function(response) {
   $('.followers span').text(response["_total"]);
 });
