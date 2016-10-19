1bf41c229c19d5baaa88896bc9cb2c99b5a8e174
Zachary Friss <friss@frissdesigns.com>
2016-10-19 01:21:33 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var clientId = "a7aag1oiocd1vxl0h9jvd2sahmaq5j0";
 
-$.getJSON( "https://api.twitch.tv/kraken/streams/moonmoon_ow?client_id="+ clientId + "callback=?", function(response) {
+$.getJSON( "https://api.twitch.tv/kraken/streams/moonmoon_ow?client_id="+ clientId + "&callback=?", function(response) {
   if (response.stream){
     $('.viewers span').text(response.stream.viewers);
     $('.intro iframe').removeClass("hidden");
@@ -10,6 +10,6 @@ $.getJSON( "https://api.twitch.tv/kraken/streams/moonmoon_ow?client_id="+ client
   }
 });
 
-$.getJSON( "https://api.twitch.tv/kraken/channels/moonmoon_ow/follows?client_id="+ clientId + "callback=?", function(response) {
+$.getJSON( "https://api.twitch.tv/kraken/channels/moonmoon_ow/follows?client_id="+ clientId + "&callback=?", function(response) {
   $('.followers span').text(response["_total"]);
 });
