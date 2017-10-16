3c4ed01dffe8bed903643d46f11b0b4ba1342ace
Alf <alf.g.jr@gmail.com>
2017-10-15 17:39:14 -0700

    Update to Twitch's video embed.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // Change this to be your own clientId from https://www.twitch.tv/kraken/oauth2/clients/new
 var clientId = "a7aag1oiocd1vxl0h9jvd2sahmaq5j0";
+var twitchUsername = "{TWITCH_USER_NAME}";
 
 $.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?client_id="+ clientId + "&callback=?", function(response) {
   if (response.stream){
     $('.viewers span').text(response.stream.viewers);
-    $('.intro iframe').removeClass("hidden");
     $('.intro-text').addClass('hidden');
   }else{
     $('.viewers span').text("0");
@@ -13,4 +13,19 @@ $.getJSON( "https://api.twitch.tv/kraken/streams/twitchusername?client_id="+ cli
 
 $.getJSON( "https://api.twitch.tv/kraken/channels/twitchusername/follows?client_id="+ clientId + "&callback=?", function(response) {
   $('.followers span').text(response["_total"]);
+});
+
+var options = {
+  channel: twitchUsername
+}
+var player = new Twitch.Player("video-player", options);
+
+player.addEventListener(Twitch.Player.ONLINE, function() {
+  $('#video-player').removeClass("hidden");
+  $('.intro-text').addClass('hidden');
+});
+
+player.addEventListener(Twitch.Player.OFFLINE, function() {
+  $('#video-player').addClass("hidden");
+  $('.intro-text').removeClass('hidden');
 });
